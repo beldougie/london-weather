99493da8ed1969136a1c054afd74f3e268ed6152
Matt Keeble <matt.keeble@gmail.com>
2018-09-22 14:42:11 +0100

    add refresh control component

--- src/components/weatherFooter.js
+++ src/components/weatherFooter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TemperatureSelector from '../containers/tempSelector';
+import RefreshController from '../containers/refresh';
 
 export default () => {
   return (
-    <div className="card-footer d-flex">
+    <div className="card-footer d-flex justify-content-between">
       <TemperatureSelector />
+      <RefreshController />
     </div>
   )
 }
---
+++ src/containers/refresh.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class RefreshControl extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshPeriod: 300,
+      interval: null,
+      periods: [
+        [60, '1 min'],
+        [300, '5 mins'],
+        [3600, '1 hour'],
+        [0, 'never']
+      ]
+    }
+  }
+
+  setInterval() {
+    if(this.state.interval !== null) clearInterval(this.state.interval);
+
+    if(this.state.refreshPeriod === 0) return this.setState({ interval: null });
+
+    const interval = setInterval(() => {
+      console.log('tick');
+    }, this.state.refreshPeriod * 1000);
+    this.setState({ interval });    
+  }
+
+  buildClickHandler(value) {
+    return () => {
+      this.setState({ refreshPeriod: value }, () => this.setInterval());
+    }
+  }
+
+  renderButton(label, value, isActive) {
+    const active = isActive ? 'active' : '';
+    return (
+      <button className={`btn btn-info ${active}`} onClick={this.buildClickHandler(value)} key={value}>
+        {label}
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div className="btn-group">
+        {this.state.periods.map(([period, label]) => {
+          return this.renderButton(label, period, period === this.state.refreshPeriod);
+        })}
+      </div>
+    );
+  }
+}
+
+export default connect()(RefreshControl);
